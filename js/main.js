e9d8bee36cb1b444bd928eeeda78d092a8ea6982
Steve Nash <steve.nash@ogilvy.com>
2019-01-21 09:36:17 +0000

    Update footer logo link to scroll to top

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 /*global $, document, window */
 $("document").ready(function() {
-    startAd();
+	startAd();
+	
+	$('a[href*="#"]').on('click', function (e) {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 500, 'linear');
+	});
+
 });
 
 
